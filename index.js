c2b9995bbff756dc5eae79d67127a4c6f1111a28
Kesse Dias <kesse.dias97@outlook.com>
2023-05-05 16:42:06 -0300

    :sparkles: feat: Add listagem de artigo e filtro de categorias

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ const Category = require("./categories/Category");
 
 //html
 app.set("view engine", "ejs");
+
+//Eixibição de artigos e categorias
 app.get("/", (req, res) => {
 
-    Article.findAll().then(articles => {
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
 
-        res.render("index", {articles: articles});
+        Category.findAll().then(categories =>{
+
+            res.render("index", {articles: articles, categories: categories});
+        });
     });
 });
 
@@ -43,6 +52,64 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.use(bodyParser.json());
+
+//Visualização do artigo pelo slug
+app.get("/:slug", (req, res)=>{
+
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+
+        if(article != undefined){
+
+            Category.findAll().then(categories =>{
+
+                res.render("article", {article: article, categories: categories});
+            });
+
+        }else{
+
+            res.redirect("/");
+        }
+
+    }).catch(err => {
+        res.redirect("/");
+    })
+});
+
+app.get("/category/:slug", (req, res)=>{
+
+    var slug = req.params.slug;
+
+    Category.findOne({
+        
+        //join
+        include: [{model: Article}],
+        where: {
+            slug: slug
+        }
+
+    }).then(category => {
+
+        if(category != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            })
+
+        }else{
+            
+            res.redirect("/");
+        }
+    }).catch(err => {
+
+        res.redirect("/");
+    })
+});
 
 app.listen(8090, () => {
     console.log("Its running");
