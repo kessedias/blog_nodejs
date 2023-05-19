b15ef1da3697a7d5c14c90852104ae7e0d2e29c0
Kesse Dias <kesse.dias97@outlook.com>
2023-05-18 21:19:33 -0300

    :bulb: chore: Adicionando comentarios

--- users/UsersController.js
+++ users/UsersController.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("./User");
 
+//rota de listagem do usuário
 router.get("/admin/users", (req, res)=>{
 
     res.send("listagem de usuários");
 });
 
+//rota do form de criação de usuário
 router.get("/admin/users/create", (req, res)=>{
 
     res.render("admin/users/create");
