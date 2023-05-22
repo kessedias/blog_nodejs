5789fe3185302a47cb1ef309d76dc92406ca1d73
Kesse Dias <kesse.dias97@outlook.com>
2023-05-22 15:52:16 -0300

    :sparkles: feat: Adicionando form de usuario

--- users/UsersController.js
+++ users/UsersController.js
@@ -14,4 +14,13 @@ router.get("/admin/users/create", (req, res)=>{
     res.render("admin/users/create");
 });
 
+//rota do form de criação de usuário
+router.post("/users/create", (req, res)=>{
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.json({email, password})
+});
+
 module.exports = router;
